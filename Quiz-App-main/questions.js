78212b343e64c72a80f67da35e40f9aefbdafaa3
faizanKhanKhan <90998806+faizanKhanKhan@users.noreply.github.com>
2023-11-03 11:49:38 +0530

    initial commit

---
+++ Quiz-App-main/questions.js
@@ -0,0 +1,48 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What is the capital of France?",
+    answer: "Paris",
+    options: [
+      "New York",
+      "London",
+      "Paris",
+      "Dublin"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who painted the Mona Lisa?",
+    answer: "Vincent Van Gogh",
+    options: [
+      "Vincent Van Gogh",
+      "Pablo Picasso",
+      "Leonardo Da Vinci",
+      "Claude Monet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  }
+    
+];
